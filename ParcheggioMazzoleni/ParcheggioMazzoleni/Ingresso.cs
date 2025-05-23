19912369cfb7121c00ebb62b6054cc0b5c087374
kappeguss <mazzolenikevin11@gmail.com>
2025-05-24 01:05:13 +0200

    Add files via upload

---
+++ ParcheggioMazzoleni/ParcheggioMazzoleni/Ingresso.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using static ParcheggioMazzoleni.Auto;
+
+namespace ParcheggioMazzoleni
+{
+    internal class Ingresso
+    {
+        private Random casuale = new Random();
+        private int tempoIngresso;
+        private List<Auto> coda;
+
+        public List<Auto> Coda { get { return coda; } }
+        public int TempoIngresso { get { return tempoIngresso; } }
+
+        public Ingresso()
+        {
+            tempoIngresso = casuale.Next(1, 5);
+            coda = new List<Auto>();
+        }
+    }
+}
+
